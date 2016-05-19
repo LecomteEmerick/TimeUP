240973e3a30e97a1068f5cc9163c0327d02067d7
Lecomte Emerick <emerick.lecomte@gmail.com>
2016-05-19 12:29:14 +0200

    GameData + Cat + Etape

---
+++ app/src/main/java/com/lechot/esgi/timeup/GameData.java
@@ -0,0 +1,20 @@
+package com.lechot.esgi.timeup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Emerick Lecomte on 19/05/2016.
+ */
+public class GameData {
+
+    public static ArrayList<Player> TeamA;
+    public static ArrayList<Player> TeamB;
+    public enum CATEGORIES { Animals, History, Games, Celebrety }
+
+    public static CATEGORIES SelectedCategories;
+
+    public static String[] AnimalsWords = new String[] { "Chat", "Chien", "Poisson", "Girafe", "Dauphin", "Singe", "Pivert", "Moineau", "Pigeons", "Poulet" };
+    public static String[] HistoryWords = new String[] { "Hitler", "Charlemagne", "Charles Martel", "Jacques Chirac", "Napolean", "Louis XVI", "Lioret" };
+    public static String[] GamesWords  = new String[] { "Darks Souls", "Minecraft", "Uncharted", "Tomb Raider", "Pokemon", "Mario", "Kirby"};
+    public static String[] CelebretyWords  = new String[] {"Mickael Jackson", "Madona", "Justin bieber", "Chuck Norris", "Emma Watson"};
+}
---
+++ app/src/main/java/com/lechot/esgi/timeup/Words.java
@@ -0,0 +1,16 @@
+package com.lechot.esgi.timeup;
+
+/**
+ * Created by Emerick Lecomte on 19/05/2016.
+ */
+public class Words {
+    public String Word;
+    public boolean isFound;
+
+    public Words(String w)
+    {
+        this.Word = w;
+        this.isFound = false;
+    }
+
+}
